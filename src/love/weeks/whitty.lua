7bec9f7ded7171c30948b099dc76c477a14a1978
EggoTheSquirrel <33180708+EggoTheSquirrel@users.noreply.github.com>
2021-08-16 19:32:06 -0500

    vanity

--- src/love/weeks/whitty.lua
+++ src/love/weeks/whitty.lua
@@ -1,7 +1,7 @@
 --[[----------------------------------------------------------------------------
 This file is part of Friday Night Funkin' Rewritten
 
-Copyright (C) 2021  HTV04
+Copyright (C) 2021  HTV04 --And EggoTheSquirrel!!!
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
